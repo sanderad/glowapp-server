3c7c89d668babf2294ccd60db847250656e81ce8
sanderad <ariasduquealexander1@gmail.com>
2026-03-17 13:57:08 -0500

    update data-source to bypass

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,13 +1,12 @@
 import { DataSource } from "typeorm";
-import { env } from "./env";
 import path from "path";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  url: env.dbUrl,
+  url: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  synchronize: false, // Migrations enabled, so sync is false
-  logging: env.nodeEnv === "development",
+  synchronize: false,
+  logging: process.env.NODE_ENV === "development",
   entities: [path.join(__dirname, "../modules/**/entities/*.entity.{ts,js}")],
   migrations: [path.join(__dirname, "../shared/migrations/*.{ts,js}")],
   subscribers: [],
